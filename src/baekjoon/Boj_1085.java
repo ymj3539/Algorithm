fcd69cbc08466526567acd1d522bf10e150ef56d
MinJuYoon <ymj3539@naver.com>
2022-07-23 23:38:37 +0900

    boj_1085_직사각형에서 탈출

---
+++ src/baekjoon/Boj_1085.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Boj_1085 {
+	static int d =0;
+	static int min = Integer.MAX_VALUE;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		
+		
+		cal(x,0);
+		cal(y,0);
+		cal(x,w);
+		cal(y,h);
+		
+		System.out.println(min);
+		
+	}
+	
+	static void cal(int a, int b) {
+		d = Math.abs(a-b);
+		min = Math.min(min, d);
+	}
+}

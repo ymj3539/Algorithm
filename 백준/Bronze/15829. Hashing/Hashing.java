1047d686efbf7cbbfb4a3b679e5d8396f924720e
minju <70592157+ymj3539@users.noreply.github.com>
2023-01-10 20:39:33 +0900

    [Bronze II] Title: Hashing, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		int L = Integer.parseInt(input.readLine());
+		int[] arr = new int[L];
+		String str = input.readLine();
+		long tmp = 0;
+		
+		for(int i=0; i<L; i++) {
+			arr[i] = str.charAt(i)-'a'+1;
+			
+			tmp += arr[i] * (Math.pow(31, i));
+		}
+		
+		tmp %= 1234567891;
+		
+		// System.out.println(Arrays.toString(arr));
+		System.out.println(tmp);
+		
+	}
+	
+}

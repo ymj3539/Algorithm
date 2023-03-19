f0c8cdeb14f392e22059cf22def61ebb5ed12a55
minju <70592157+ymj3539@users.noreply.github.com>
2023-03-19 19:52:04 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.24 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        // 왼손 위치 1차원 배열
+        int[] lefthand = {3,0};
+        // 오른손 위치 1차원 배열
+        int[] righthand = {3,2};
+        // 키패드 위치 2차원 배열
+        int[][] keypad = {
+            {3,1}, // 0 
+            {0,0}, // 1
+            {0,1}, // 2
+            {0,2}, // 3
+            {1,0}, // 4
+            {1,1}, // 5
+            {1,2}, // 6
+            {2,0}, // 7
+            {2,1}, // 8
+            {2,2}, // 9
+            {3,0}, //10(*)
+            {3,1}, //11(#)
+        };
+        
+        
+        for(int i=0; i<numbers.length; i++){
+            // 왼쪽 번호일 때
+            if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7){
+                // 문자열에 L추가
+                sb.append("L");
+                // 왼손 위치 갱신
+                lefthand = keypad[numbers[i]];
+            }
+            // 오른쪽 번호일 때
+            else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9){
+                // 문자열에 R추가
+                sb.append("R");
+                // 오른손 위치 갱신
+                righthand = keypad[numbers[i]];
+            }
+            // 중앙 번호일 때
+            else{
+                // 왼손과의 위치 비교
+                int dl = Math.abs(lefthand[0] - keypad[numbers[i]][0]) + Math.abs(lefthand[1] - keypad[numbers[i]][1]);
+                // 오른손과의 위치 비교
+                int dr = Math.abs(righthand[0] - keypad[numbers[i]][0]) + Math.abs(righthand[1] - keypad[numbers[i]][1]);
+                // 왼손 오른손 비교
+                // 왼손이 더 가까우면
+                if(dl < dr){
+                    // 문자열에 L추가
+                    sb.append("L");
+                    // 왼손 위치 갱신
+                    lefthand = keypad[numbers[i]];
+                }else if(dr < dl){
+                    // 문자열에 R추가
+                    sb.append("R");
+                    // 오른손 위치 갱신
+                    righthand = keypad[numbers[i]];
+                }else{
+                    if(hand.equals("right")){
+                        // 문자열에 R추가
+                        sb.append("R");
+                        // 오른손 위치 갱신
+                        righthand = keypad[numbers[i]];
+                    }else{
+                        // 문자열에 L추가
+                        sb.append("L");
+                        // 왼손 위치 갱신
+                        lefthand = keypad[numbers[i]];
+                    }
+                }
+            
+            }
+        }        
+        answer = sb.toString();
+        return answer;
+    }
+}

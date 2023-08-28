9543a871ac600cf8870d4d850cb897faf50145bf
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2023-08-28 15:13:11 +0900

    [Gold II] Title: Maaaaaaaaaze, Time: 1164 ms, Memory: 298924 KB -BaekjoonHub

--- 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.java
+++ 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.java
@@ -1,162 +1,138 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer tokens;
-	
-	static int[][][] map, copymap;
-	static int min = Integer.MAX_VALUE;
-	static int dx[] = {0, 0, 0, 0, 1, -1};
-	static int dy[] = {0, 0, 1, -1, 0, 0};	
-	static int dz[] = {1, -1, 0, 0, 0, 0};	
-	public static void main(String[] args) throws IOException {
-		
-		map = new int[5][5][5];
-		
-		for(int i=0; i<5; i++) {
-			for(int j=0; j<5; j++) {
-				tokens = new StringTokenizer(input.readLine());
-				for(int k=0; k<5; k++) {
-					map[i][j][k] = Integer.parseInt(tokens.nextToken());
-				}
-			}
-		}
-		
-//		for(int[][] i : map) {
-//			for(int[] j: i) {
-//				System.out.println(Arrays.toString(j));
-//			}
-//		}
-		/////// 입력끝 ///////////
-		
-		makePerm(0, new int[5], new boolean[5]);
-		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
-	}
-	
-	// 쌓는 순서 순열 만들기
-	static void makePerm(int nth, int[] choosed, boolean[] visited) {
-		if(nth == 5) {
-			// 4방향으로 돌리기
-			makeDupPerm(0, new int[5], choosed);
-			return;
-		}
-		
-		for(int i=0; i<5; i++) {
-			if(!visited[i]) {
-				choosed[nth] = i;
-				visited[i] = true;
-				makePerm(nth+1, choosed, visited);
-				visited[i] = false;
-			}
-		}
-	}
-	
-	// 회전 방향 정하기 - 중복 순열
-	static void makeDupPerm(int nth, int[] dirChoosed, int[] choosed) {
-		if(nth == 5) {
-			copymap = new int[5][5][5];
-			for(int i=0; i<5; i++) {
-				copymap[i] = map[choosed[i]];
-			}
-			
-			// 회전시키기
-			for(int i=0; i<5; i++) {
-				copymap[i] = rotate(dirChoosed[i], copymap[i]);
-			}
-			
-			// 출입구가 막혔으면 return
-			if(copymap[0][0][0] == 0 || copymap[4][4][4] == 0) {
-				return;
-			}
-			
-			min = Math.min(bfs(copymap), min);
-			return;
-		}
-		
-		for(int i=1; i<=4; i++) {
-			dirChoosed[nth] = i;
-			makeDupPerm(nth+1, dirChoosed, choosed);
-		}
-	}
-	
-	// 판 회전시키기
-	static int[][] rotate(int n, int[][] copymap){
-		
-		for(int i=0; i<n; i++) {
-			int[][] tmp = new int[5][5];
-			for(int r=4; r>=0; r--) {
-				for(int c=0; c<5; c++) {
-					tmp[r][c] = copymap[c][4-r];
-				}
-			}
-			copymap = tmp;
-		}
-		
-		return copymap;
-	}
-	
-	// 미로 탐색
-	static int bfs (int[][][] copymap) {
-		Queue<Point> queue = new LinkedList<>();
-		boolean[][][] visited = new boolean[5][5][5];
-		
-		queue.offer(new Point(0,0,0));
-		visited[0][0][0] = true;
-		int cnt =0;
-		
-		while(!queue.isEmpty()) {
-			int size = queue.size();
-			
-			while(size-- >0) {
-				Point cur = queue.poll();
-				if(cur.z==4 &&  cur.y==4 && cur.x == 4 ) return cnt;
-				int nz = 0;
-				int ny = 0;
-				int nx = 0;
-				
-				for(int i=0; i<6; i++) {
-					nz = cur.z + dz[i];
-					ny = cur.y + dy[i];
-					nx = cur.x + dx[i];
-					
-					if(isIn(nz, ny, nx) && !visited[nz][ny][nx] && copymap[nz][ny][nx] == 1) {
-						queue.offer(new Point(nz, ny, nx));
-						visited[nz][ny][nx] = true;
-					}
-				}
-				
-			}
-			cnt++;
-		}
-		
-		return min;
-		
-	}
-	
-	static boolean isIn(int z, int y, int x) {
-		return (z>=0 && y>=0 && x>=0 && z<5 && y<5 && x<5);
-	}
-	
-	static class Point{
-		int z,y,x;
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static int[][][] maze = new int[5][5][5];
+    static int Min = Integer.MAX_VALUE;
+    static int[][] deltas = {
+            {0,0,-1},
+            {-1,0,0},
+            {0,1,0},
+            {0,0,1},
+            {1,0,0},
+            {0,-1,0}
+    };
+    public static void main(String[] args) throws Exception{
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                tokens = new StringTokenizer(input.readLine());
+                for(int k=0; k<5; k++){
+                    maze[i][j][k] = Integer.parseInt(tokens.nextToken());
+                }
+            }
+        }
 
-		public Point(int z, int y, int x) {
-			this.z = z;
-			this.y = y;
-			this.x = x;
-		}
+//        for(int i=0; i<5; i++){
+//            for(int[] j : maze[i]){
+//                System.out.println(Arrays.toString(j));
+//            }
+//        }
 
-		@Override
-		public String toString() {
-			return "(" + z + ", " + y + ", " + x + ")";
-		}
-	}
-	
-	
+
+        // 판 쌓기
+        perm(0, new int[5], new boolean[5]);
+
+        System.out.println(Min == Integer.MAX_VALUE ? -1 : Min);
+
+
+    }
+
+    static void perm(int nth, int[] choosed, boolean[] visited) {
+        if(nth == 5){
+            // 순서대로 판 쌓기
+            int[][][] new_maze = new int[5][5][5];
+            for(int i=0; i<5; i++){
+                new_maze[i] = maze[choosed[i]];
+            }
+
+            // 회전 시키기
+            turn(0, new_maze);
+
+            return;
+        }
+
+        for(int i=0; i<5; i++){
+            if(!visited[i]){
+                choosed[nth] = i;
+                visited[i] = true;
+                perm(nth+1, choosed, visited);
+                visited[i] = false;
+            }
+        }
+    }
+
+    static void turn(int nth, int[][][] maze){
+        if(nth == 5){
+            // bfs 탐색
+            if(maze[0][0][0] == 0) return;
+            int depth = bfs(maze);
+            Min = Math.min(depth, Min);
+            return;
+        }
+
+        // nth번째 판의 방향 정하기
+        for(int i=0; i<4; i++){
+            int[][] new_arr = new int[5][5];
+            for(int r=0; r<5; r++){
+                for(int c=0; c<5; c++){
+                    new_arr[c][5-1-r] = maze[nth][r][c];
+                }
+            }
+
+            maze[nth] = new_arr;
+            turn(nth+1, maze);
+        }
+
+
+    }
+
+    static int bfs(int[][][] maze){
+        Queue<Point> queue = new LinkedList();
+        boolean[][][] visited = new boolean[5][5][5];
+        queue.offer(new Point(0,0,0));
+        visited[0][0][0] = true;
+
+        int depth = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while(size-->0){
+                Point cur = queue.poll();
+                if(cur.r == 4 && cur.c == 4 && cur.z == 4){
+                    return depth;
+                }
+
+                for(int i=0; i<6; i++){
+                    int dr = cur.r + deltas[i][0];
+                    int dc = cur.c + deltas[i][1];
+                    int dz = cur.z + deltas[i][2];
+
+                    if(!isIn(dr, dc, dz)) continue;
+                    if(visited[dr][dc][dz]) continue;
+                    if(maze[dr][dc][dz] == 0) continue;
+
+                    queue.offer(new Point(dr, dc, dz));
+                    visited[dr][dc][dz] = true;
+                }
+            }
+
+            depth++;
+        }
+
+        return Integer.MAX_VALUE;
+    }
+
+    static boolean isIn(int r, int c, int z){
+        return (r>=0 && c>=0 && z>=0 && r<5 && c<5 && z<5);
+    }
+
+    static class Point{
+        int r, c, z;
+        public Point(int r, int c, int z){
+            this.r = r;
+            this.c = c;
+            this.z = z;
+        }
+    }
+
 }

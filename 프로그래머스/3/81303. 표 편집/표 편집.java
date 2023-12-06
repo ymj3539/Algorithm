498cad3f2c35aafa058137f5d23064bb845cab68
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2023-12-06 19:48:45 +0900

    [level 3] Title: 표 편집, Time: 337.18 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,120 +1,119 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    static class Node{
-        Node pre; // 앞 노드
-        Node next; //뒷 노드
-        boolean isDelete; // 삭제 되었니?
+    static class Node {
+        Node pre;
+        int ele;
+        Node next;
+        boolean isExist;
         
-        public Node(Node pre, Node next, boolean isDelete){
-            this.pre = pre;
-            this.next = next;
-            this.isDelete = isDelete;
+        public Node(Node p, int e, Node n, boolean i){
+            this.pre = p;
+            this.ele = e;
+            this.next = n;
+            this.isExist = i;
         }
-        
     }
     
+    Node[] list;
+    Node cur;
+    Stack<Node> stack;
+    
     public String solution(int n, int k, String[] cmd) {
-        Node[] list = new Node[n];
+        String answer = "";
         
-        list[0] = new Node(null, null, false);
+        list = new Node[n];
+        stack = new Stack<>();
+        
+        
+        list[0] = new Node(null, 0, null, true);
         for(int i=1; i<n; i++){
-            list[i] = new Node(list[i-1], null, false);
+           list[i] = new Node(list[i-1], i, null, true);
             list[i-1].next = list[i];
         }
         
-        Stack<Node> stack = new Stack<>();
-        Node cur  = list[k];
+        cur = list[k];
         
-        int size = cmd.length;
-        for(int i=0; i<size; i++){
+        for(int i=0; i<cmd.length; i++){
+            StringTokenizer tokens = new StringTokenizer(cmd[i]);
+            String cmd_l = tokens.nextToken();
             
-            String[] str = cmd[i].split(" ");
-            // D일때
-            if(str[0].equals("D")){
-                // 주어진 수만큼 next쪽으로 이동
-                for(int j=0; j<Integer.parseInt(str[1]); j++){
-                    cur = cur.next;
+            if(cmd_l.equals("U")){
+                int X = Integer.parseInt(tokens.nextToken());
+                
+                for(int j=0; j<X; j++){
+                        cur = cur.pre;
+                }
+                
+            }
+            
+            else if(cmd_l.equals("D")){
+                int X = Integer.parseInt(tokens.nextToken());
+                
+                for(int j=0; j<X; j++){
+                        cur = cur.next;    
                 }
             }
             
-//             // U 일때
-            else if(str[0].equals("U")){
-                // 주어진 수만큼 pre쪽으로 이동
-                for(int j=0; j<Integer.parseInt(str[1]); j++){
-                    cur = cur.pre;
-                }
-            }
-            
-//             // C 일때
-            else if(str[0].equals("C")){
-                // 스택에 넣고 삭제되었다고 표시
+            else if(cmd_l.equals("C")){
+                cur.isExist = false;
                 stack.push(cur);
-                cur.isDelete = true;
                 
-                // 현재 노드의 앞노드와 뒷노드
-                Node pre = cur.pre;
-                Node next = cur.next;
-                
-                // 맨 앞이 아니라면
-                if(pre != null){
-                    // 현재 노드의 앞노드와 뒷노드를 연결
-                    pre.next = next;
+                if(cur.pre == null){
+                    cur = cur.next;
+                    cur.pre = null;
                 }
                 
-                // 맨 뒤가 아니라면
-                if(next != null){
-                    // 현재 노드의 뒷노드와 앞노드를 연결
-                    next.pre = pre;
-                    // 가리키는 행은 그 다음 행
-                    cur = next;
+                else{
+                    // 삭제 행이 가장 마지막 행일 때
+                    if(cur.next == null){
+                        cur = cur.pre;
+                        cur.next = null;
+                    }else{
+                        cur.pre.next = cur.next;
+                        cur.next.pre = cur.pre;
+                        cur = cur.next;
+                    }
+                }
+                
+            }
+            
+            // Z 명령어 실행(실행 취소)
+            else {
+                Node rollback = stack.pop();
+                list[rollback.ele].isExist = true;
+                
+                // 맨 앞일때
+                if(rollback.pre == null){
+                    rollback.next.pre = rollback;
                 }
                 else {
-                    // 맨 뒤라면
-                    // 가리키는 행은 그 이전 행
-                    cur = pre;
+                    // 맨 끝일때
+                    if(rollback.next == null){
+                        rollback.pre.next = rollback;
+                    }else {
+                        rollback.pre.next = rollback;
+                        rollback.next.pre = rollback;
+                    }    
                 }
                 
-            }
-            
-//             // Z 일때 (삭제 취소)
-            else if(str[0].equals("Z")){
-                // 스택에서 빼고 삭제 표시 바꿔줌
-                Node node = stack.pop();
-                node.isDelete = false;
                 
-                Node pre = node.pre;
-                Node next = node.next;
                 
-                // 맨 앞이 아니라면
-                if(pre != null){
-                    // 앞 노드의 next와 삭제 취소 노드 연결
-                    pre.next = node;
-                }
-                
-                // 맨뒤가 아니라면
-                if(next != null){
-                    // 뒷 노드의 pre와 삭제 취소 노드를 연결
-                    next.pre = node;
-                }
-                /// 삭제 취소 작업에서 현재 가리키는 행은 수정하지 않음
             }
         }
-    
+        
         StringBuilder sb = new StringBuilder();
-        
-        // list 배열 돌면서 삭제된 노드는 X 표시
         for(int i=0; i<n; i++){
-            if(list[i].isDelete){
-                sb.append("X");
-            }else {
+            if(list[i].isExist){
                 sb.append("O");
-            }
+                
+            }else sb.append("X");
         }
-        
-        String answer = "";
         answer = sb.toString();
+        
         return answer;
+        
+        
     }
 }

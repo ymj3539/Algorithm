f41cd0450cbc5360d881edd02660fe845510f3aa
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2024-01-16 14:09:12 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 212 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,76 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-
-
+import java.util.*;
+import java.io.*;
 public class Main {
-	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer tokens;
-	
-	static int N;
-	static int[][] map;
-	static int[] dx = {1, 1, 0}; // 우(0), 대각선(1), 하(2)
-	static int[] dy = {0, 1, 1};
-	static int[][] pipes = {
-			{0, 1},
-			{0, 1, 2},
-			{1, 2}
-	};
-	static int cnt;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		N = Integer.parseInt(input.readLine());
-		map = new int[N+1][N+1];
-		for(int r=1; r<=N; r++) {
-			tokens = new StringTokenizer(input.readLine());
-			for(int c=1; c<=N; c++) {
-				map[r][c] = Integer.parseInt(tokens.nextToken());
-			}
-		}
-		
-//		for(int[] i: map) {
-//			System.out.println(Arrays.toString(i));
-//		}
-		
-		dfs(1, 2, 0);
-		System.out.println(cnt);
-	}
-	
-	static void dfs(int r, int c, int type) {
-		if(r==N && c==N) {
-			cnt++;
-			return;
-		}
-		
-		for(int i=0; i<pipes[type].length; i++) {
-			int dr = r + dy[pipes[type][i]];
-			int dc = c + dx[pipes[type][i]];
-			
-			// 범위 안에 있는지 체크
-			if(!isIn(dr,dc)) continue;
-			// 벽이 있는지 체크
-			if(!canGo(dr, dc, pipes[type][i])) continue;
-			
-			dfs(dr, dc, pipes[type][i]);
-		}
-	}
-	
-	static boolean canGo(int r, int c, int type) {
-		// 가로 파이프일때
-		if(type == 0) {
-			if(map[r][c] == 1) return false;
-		}else if(type== 1) {
-			if(map[r][c] ==1 || map[r][c-1] ==1 || map[r-1][c] == 1) return false;
-		}else if(type == 2){
-			if(map[r][c] == 1) return false;
-		}
-		return true;
-	}
-	
-	static boolean isIn(int r, int c) {
-		return (r>0 && c>0 && r<=N && c<=N);
-	}
-	
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static int N;
+    static int[][] map;
+    static int[] dy = {0, 1, 1};
+    static int[] dx = {1, 0, 1};
+    static int CNT;
+    public static void main(String[] args) throws Exception{
+        N = Integer.parseInt(input.readLine());
+
+        map = new int[N+1][N+1];
+
+        for(int r=1; r<=N; r++){
+            tokens = new StringTokenizer(input.readLine());
+            for(int c=1; c<=N; c++){
+                map[r][c] = Integer.parseInt(tokens.nextToken());
+            }
+        }
+
+        dfs(1, 1, 1, 2, 0);
+        System.out.println(CNT);
+    }
+
+    static void dfs(int r1, int c1, int r2, int c2, int state) {
+        if (r2 == N && c2 == N) {
+            CNT++;
+            return;
+        }
+
+        // 가로일때
+        if (state == 0) {
+            // 오른쪽으로 이동
+            if(isIn(r2+dy[0], c2+dx[0])){
+                dfs(r2, c2, r2+dy[0], c2+dx[0], 0);
+            }
+
+            // 대각선으로 이동
+            if(isIn(r2+dy[2], c2+dx[2]) && canGo(r1, c1, r2, c2)){
+                dfs(r2, c2, r2+dy[2], c2+dx[2], 2);
+            }
+
+        }
+        // 세로일때
+        else if (state == 1) {
+            // 밑으로 이동
+            if(isIn(r2+dy[1], c2+dx[1])){
+                dfs(r2, c2, r2+dy[1], c2+dx[1], 1);
+            }
+
+            // 대각선으로 이동
+            if(isIn(r2+dy[2], c2+dx[2]) && canGo(r1, c1, r2, c2)){
+                dfs(r2, c2, r2+dy[2], c2+dx[2], 2);
+            }
+        }
+        // 대각선일때
+        else {
+            // 오른쪽으로 이동
+            if(isIn(r2+dy[0], c2+dx[0])){
+                dfs(r2, c2, r2+dy[0], c2+dx[0], 0);
+            }
+            // 밑으로 이동
+            if(isIn(r2+dy[1], c2+dx[1])){
+                dfs(r2, c2, r2+dy[1], c2+dx[1], 1);
+            }
+
+            // 대각선으로 이동
+            if(isIn(r2+dy[2], c2+dx[2]) && canGo(r1, c1, r2, c2)){
+                dfs(r2, c2, r2+dy[2], c2+dx[2], 2);
+            }
+        }
+    }
+
+    static boolean canGo(int r1, int c1, int r2, int c2){
+        return (map[r2+dy[0]][c2+dx[0]] == 0 && map[r2+dy[1]][c2+dx[1]]==0);
+    }
+
+    static boolean isIn(int r, int c){
+        return r>=1 && r<=N && c>=1 && c<=N && map[r][c] != 1;
+    }
 }

a597a0551dfe20ec06bb993206379f8b415d1d3a
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2024-02-29 00:46:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 148 ms, Memory: 51296 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,32 +4,46 @@ public class Main {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer tokens;
     static int N, K;
-    public static void main(String[] args)throws Exception{
+    static Obj[] objects;
+    public static void main(String[] args) throws Exception{
         tokens = new StringTokenizer(input.readLine());
-
         N = Integer.parseInt(tokens.nextToken());
         K = Integer.parseInt(tokens.nextToken());
 
-        int[] W = new int[N+1];
-        int[] V = new int[N+1];
-        for(int i=1; i<=N; i++){
+        objects = new Obj[N+1];
+
+        for(int i=1; i<=N; i++) {
             tokens = new StringTokenizer(input.readLine());
-            W[i] = Integer.parseInt(tokens.nextToken());
-            V[i] = Integer.parseInt(tokens.nextToken());
+            int w = Integer.parseInt(tokens.nextToken());
+            int v = Integer.parseInt(tokens.nextToken());
+
+            objects[i] = new Obj(w, v);
         }
 
+        // i번째 아이템까지 담을 수 있고, 최대 j무게까지 담을 수 있을 때의 최대 value
         int[][] dp = new int[N+1][K+1];
 
         for(int i=1; i<=N; i++){
-            for(int w=i; w<=K; w++){
-                dp[i][w] = dp[i-1][w];
-                if(W[i] <= w){
-                    dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w - W[i]] + V[i]);
+            for(int j=0; j<=K; j++){
+                if(j >= objects[i].w){
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-objects[i].w] + objects[i].v);
+                }else {
+                    dp[i][j] = dp[i-1][j];
                 }
 
             }
         }
 
         System.out.println(dp[N][K]);
+
+    }
+
+    static class Obj{
+        int w, v;
+        public Obj(int w, int v){
+            this.w = w;
+            this.v = v;
+        }
+
     }
 }

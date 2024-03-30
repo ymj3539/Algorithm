4a859f93fc97208fd90c2c3f930930248c90d235
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2024-03-30 18:23:25 +0900

    [Gold V] Title: 상어 초등학교, Time: 196 ms, Memory: 19964 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
@@ -5,146 +5,122 @@ public class Main {
     static StringTokenizer tokens;
     static int N;
     static int[][] map;
-    static int[][] students;
-    static int[][] stu_map;
-    static int[] dy = {0, 0, 1, -1};
-    static int[] dx = {1, -1, 0, 0};
+    static int[][] favorite;
+    static int[] dy = {-1, 0, 0, 1};
+    static int[] dx = {0, -1, 1, 0};
     public static void main(String[] args) throws Exception{
         N = Integer.parseInt(input.readLine());
         map = new int[N+1][N+1];
-        students = new int[N*N+1][4];
-        stu_map = new int[N*N+1][2]; // 학생 번호, 학생 좌표{r,c}
 
+        favorite = new int[N*N+1][5];
         for(int i=1; i<=N*N; i++){
             tokens = new StringTokenizer(input.readLine());
-            int student = Integer.parseInt(tokens.nextToken());
-            students[student][0] = Integer.parseInt(tokens.nextToken());
-            students[student][1] = Integer.parseInt(tokens.nextToken());
-            students[student][2] = Integer.parseInt(tokens.nextToken());
-            students[student][3] = Integer.parseInt(tokens.nextToken());
-            case1(student);
-        }
-
-
-        int total = 0;
-        // 만족도 계산
-        for(int r=1; r<=N; r++){
-            for(int c=1; c<=N; c++){
-                int point = isGood(r,c);
-                if(point == 1) total += 1;
-                else if(point == 2) total += 10;
-                else if(point == 3) total += 100;
-                else if(point == 4) total += 1000;
-            }
-        }
-
-        System.out.println(total);
-
-    }
-
-    static int isGood(int r,int c){
-        int stu_num = map[r][c];
-        int cnt = 0;
-        for(int i=0; i<4; i++){
-            int dr = r+dy[i];
-            int dc = c+dx[i];
-            if(!isIn(dr,dc)) continue;
-
+            int stu_num = Integer.parseInt(tokens.nextToken());
             for(int j=0; j<4; j++){
-               if(students[stu_num][j] == map[dr][dc]) cnt++;
+                favorite[stu_num][j] = Integer.parseInt(tokens.nextToken());
             }
+            sit(stu_num);
         }
-        return cnt;
+
+        // 만족도 계산
+        System.out.println(cal());
+
+
     }
-
-
-
-    static void case1(int stu_num){
-        int[][] cnt_map = new int[N+1][N+1];
-        int[][] empty_map = new int[N+1][N+1];
-        int cnt_max = Integer.MIN_VALUE;
+    static int cal(){
+        int sum = 0;
         for(int r=1; r<=N; r++){
             for(int c=1; c<=N; c++){
-                // 비어있는 칸이면
-                if(map[r][c] == 0){
-                    // 인접한 곳에 선호하는 숫자가 있는지 확인
-                    int cnt = 0;
-                    int empty = 0;
+                int cnt = 0;
+                int stu_num = map[r][c];
+                for(int i=0; i<4; i++){
+                    int dr = r + dy[i];
+                    int dc = c + dx[i];
 
+                    if(!isIn(dr, dc)) continue;
+                    for(int s=0; s<4; s++){
+                        if(favorite[stu_num][s] == map[dr][dc]) {
+                            cnt++;
+                            break;
+                        }
+                    }
+                }
+
+                if(cnt!=0) sum += Math.pow(10, (cnt-1));
+            }
+        }
+
+        return sum;
+    }
+
+    static void sit(int stu){
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        // 빈자리 탐색
+        for(int r=1; r<=N; r++){
+            for(int c=1; c<=N; c++){
+                // 빈자리라면
+                if(map[r][c] == 0){
+                    // 주변에 선호하는 학생 몇명인지 탐색
+                    int fav_stu_cnt = 0;
+                    int empty = 0;
                     for(int i=0; i<4; i++){
                         int dr = r + dy[i];
                         int dc = c + dx[i];
 
-                        if(!isIn(dr,dc)) continue;
+                        if(!isIn(dr, dc)) continue;
                         if(map[dr][dc] == 0) {
                             empty++;
                             continue;
                         }
-
-                        for(int j=0; j<4; j++){
-                            if(map[dr][dc] == students[stu_num][j]) cnt++;
+                        for(int s=0; s<4; s++){
+                            if(favorite[stu][s] == map[dr][dc]) {
+                                fav_stu_cnt++;
+                                break;
+                            }
                         }
+
                     }
-
-                    cnt_max = Math.max(cnt_max, cnt);
-                    cnt_map[r][c] = cnt;
-                    empty_map[r][c] = empty;
+                    // pq에 담기
+                    pq.offer(new Point(r, c, fav_stu_cnt, empty));
                 }
             }
         }
 
-
-        // 선호하는 학생이 제일 많이 인접한 곳 찾기
-        Queue<int[]> cnt_q = new LinkedList();
-
-        PriorityQueue<int[]> empty_q = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2){
-                if(o1[0] == o2[0]){
-                    return o1[1] - o2[1];
-                }else{
-                    return o1[0] - o2[0];
-                }
-            }
-        });
-
-        for(int r=1; r<=N; r++){
-            for(int c=1; c<=N; c++){
-                // map에 이미 숫자가 있는 경우 다시 걸러줘야돼!!!
-                if(cnt_map[r][c] == cnt_max && map[r][c] == 0) cnt_q.offer(new int[] {r, c});
-            }
-        }
-
-        if(cnt_q.size() > 1){
-            // case 2번 조건으로 넘어가
-
-            int max = Integer.MIN_VALUE;
-            while(!cnt_q.isEmpty()){
-                // empty값 비교
-                int[] cur = cnt_q.poll();
-                int cr = cur[0];
-                int cc = cur[1];
-                if(empty_map[cr][cc] > max){
-                    max = empty_map[cr][cc];
-                    empty_q.clear();
-                    empty_q.offer(new int[] {cr, cc});
-                }else if(empty_map[cr][cc] == max){
-                    empty_q.offer(new int[] {cr, cc});
-                }
-            }
-            if(!empty_q.isEmpty()){
-                map[empty_q.peek()[0]][empty_q.peek()[1]] = stu_num;
-            }
-        }else if(cnt_q.size() == 1){
-            // 조건 맞는게 1개면 좌표에 해당 학생 번호 넣기
-            map[cnt_q.peek()[0]][cnt_q.peek()[1]] = stu_num;
+        // pq에서 하나 꺼내서 맵에 체크
+        if(!pq.isEmpty()){
+            Point cur = pq.poll();
+            map[cur.r][cur.c] = stu;
         }
 
     }
 
+    static boolean isIn(int r, int c) {
+        return r>=1 && r<=N && c>=1 && c<=N;
+    }
 
+    static class Point implements Comparable<Point>{
+        int r, c;
+        int fav_cnt;
+        int empty_cnt;
+        public Point(int r, int c, int f, int e){
+            this.r = r;
+            this.c = c;
+            this.fav_cnt = f;
+            this.empty_cnt = e;
+        }
 
-    static boolean isIn(int r, int c){
-        return (r>=1 && r<=N && c>=1 && c<=N);
+        @Override
+        public int compareTo(Point p){
+            if(this.fav_cnt == p.fav_cnt){
+                if(this.empty_cnt == p.empty_cnt){
+                    if(this.r == p.r) {
+                        return this.c - p.c;
+                    }
+                    return this.r - p.r;
+                }
+                return p.empty_cnt - this.empty_cnt;
+            }
+            return p.fav_cnt - this.fav_cnt;
+        }
     }
 }

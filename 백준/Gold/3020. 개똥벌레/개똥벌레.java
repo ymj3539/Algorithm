b11d82e8fc6217ebc9bd84d3b3bba818a523e613
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2024-04-20 08:30:48 +0900

    [Gold V] Title: 개똥벌레, Time: 472 ms, Memory: 33864 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static int N, H;
+
+    public static void main(String[] args) throws Exception {
+        tokens = new StringTokenizer(input.readLine());
+
+        N = Integer.parseInt(tokens.nextToken());
+        H = Integer.parseInt(tokens.nextToken());
+
+        int[] bottom = new int[N/2];
+        int[] top = new int[N/2];
+
+        for(int i=0; i<N/2; i++){
+            bottom[i] = Integer.parseInt(input.readLine());
+            top[i] = Integer.parseInt(input.readLine());
+        }
+
+        Arrays.sort(bottom);
+        Arrays.sort(top);
+
+        int min = Integer.MAX_VALUE;    // 장애물의 최솟값
+        int ans_cnt = 0;    // 구간의 수
+
+        for(int r=1; r<=H; r++){
+            int tmp_cnt = 0; // 해당 높이에서 파괴할 수 있는 장애물 갯수
+             // 이진 탐색
+            tmp_cnt += count(top, r);
+            tmp_cnt += count(bottom, H+1-r);
+
+            // 최솟값 비교
+            // 최솟값보다 작으면 -> 최솟값 갱신, 구간의 수 갱신
+            if(min > tmp_cnt){
+                min = tmp_cnt;
+                ans_cnt = 1;
+            }
+            // 최솟값과 같으면 구간의 수 증가
+            else if(min == tmp_cnt){
+                ans_cnt++;
+            }
+        }
+
+        System.out.println(min+" "+ans_cnt);
+    }
+
+    static int count(int[] arr, int h){
+        int left = 0;
+        int right = N/2;
+        int mid;
+
+        while(left < right){
+
+            mid = (left + right)/2;
+            if(arr[mid] >= h){
+                right = mid;
+            }else if(arr[mid] < h){
+                left = mid+1;
+            }
+        }
+
+        return N/2 - right;
+    }
+}

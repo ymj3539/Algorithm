24e0c833b0c809349c207dd8db14c1cae82b0f10
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2024-05-15 00:07:14 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,9 @@
 -- 코드를 입력하세요
-select i.name as NAME, i.datetime as DATETIME
-from animal_ins i left join animal_outs o
+select i.name, i.datetime
+from animal_ins i
+left join animal_outs o
 on i.animal_id = o.animal_id
 where o.animal_id is null
-order by i.datetime asc
+order by i.datetime
 limit 3;
+

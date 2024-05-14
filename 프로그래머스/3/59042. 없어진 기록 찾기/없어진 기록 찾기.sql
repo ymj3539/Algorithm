f05f851e83529170513347b124e023caec1e06ad
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2024-05-14 22:27:43 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,24 @@
 -- 코드를 입력하세요
-select o.animal_id as ANIMAL_ID, o.name as NAME
-from animal_ins i right join animal_outs o
+select o.animal_id, o.name
+from animal_ins i
+right join animal_outs o
 on i.animal_id = o.animal_id
 where i.animal_id is null
-order by o.animal_id asc;
+order by o.animal_id;
+
+
+
+
+
+
+
+
+
+
+
+
+# select o.animal_id as ANIMAL_ID, o.name as NAME
+# from animal_ins i right join animal_outs o
+# on i.animal_id = o.animal_id
+# where i.animal_id is null
+# order by o.animal_id asc;

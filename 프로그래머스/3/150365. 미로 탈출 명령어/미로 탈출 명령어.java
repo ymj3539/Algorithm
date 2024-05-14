6d062cf381e8cc49e5491e32ac194aadd7b89a42
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2024-05-14 18:04:39 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 7.79 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,73 +1,82 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 class Solution {
+    static int N, M, K;
+    static int[][] map;
+    static Point start, end;
     static int[] dy = {1, 0, 0, -1};
     static int[] dx = {0, -1, 1, 0};
-    static String[] str_arr = {"d", "l", "r", "u"};
-    static int N, M, X, Y, R, C, K;
-    static String Answer = "impossible";
-    static boolean Flag;
+    static char[] strArr = {'d', 'l', 'r', 'u'};
+    static String Ans = "impossible";
+    static boolean flag = false;
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         String answer = "";
+         
         N = n;
         M = m;
-        X = x-1;
-        Y = y-1;
-        R = r-1;
-        C = c-1;
+        map = new int[N+1][M+1];
+        
+        start = new Point(x, y);
+        end = new Point(r, c);
         K = k;
         
-        // 최단 경로의 길이 구하기
-        int dis = checkDistance(R, C, X, Y);
+        boolean[][][] visited= new boolean[K+1][N+1][M+1];
+        visited[0][start.r][start.c] = true;
+        char[] arr = new char[K];
         
-        // 최단경로가 K보다 크거나, K - dis 가 홀수면 impossible
-        if( dis > K ||  ((K-dis) % 2 ==1)) answer = "impossible";
-        else {
-            dfs("", X, Y);
-            answer = Answer;
+        // 최단 경로의 길이
+        int dis = checkMin(start.r, start.c, end.r, end.c);
+        if(dis > K || (K-dis) %2 == 1) {
+            
+        }else {
+            dfs(0, start.r, start.c, visited, arr);
         }
         
         
+        answer = Ans;
         return answer;
     }
     
-    static int checkDistance(int r, int c, int x, int y){
-        return Math.abs(r-x) + Math.abs(c-y);
-    }
-    
-    static void dfs(String str, int r, int c){
-        if(Flag) return;
-        // 백트래킹 추가 : (지나온 거리 + 현재 위치~최단거리) > K 면 더 진행안해도 됨(어차피 못감)
-        if(str.length() + checkDistance(r, c, R, C) > K) return;
-        if(str.length() == K){
-            if(r == R && c == C){
-                Answer = str;
-                Flag = true;
+    static void dfs(int nth, int r, int c, boolean[][][] visited, char[] path){
+        if(flag) return;
+        if(nth + checkMin(r, c, end.r, end.c) > K) return;
+        if(nth == K){
+            if(r == end.r && c == end.c){
+                Ans = String.valueOf(path);
+                flag = true;
             }
+            
             return;
         }
         
         for(int i=0; i<4; i++){
             int dr = r + dy[i];
             int dc = c + dx[i];
+            
             if(!isIn(dr, dc)) continue;
-            dfs(str+str_arr[i], dr, dc);
+            
+            if(!visited[nth][dr][dc]){
+                visited[nth][dr][dc] = true;
+                path[nth] = strArr[i];
+                dfs(nth+1, dr, dc, visited, path);
+                visited[nth][dr][dc] = false;
+            }
         }
-        
+    }
+    
+    static int checkMin(int r, int c, int rr, int cc){
+        return Math.abs(r-rr) + Math.abs(c-cc);
     }
     
     static boolean isIn(int r, int c){
-        return r>=0 && r<N && c>=0 && c<M;
+        return r>=1 && r<=N && c>=1 && c<=M;
     }
     
     static class Point{
         int r, c;
-        String path;
-        public Point(int r, int c, String path){
+        public Point(int r, int c){
             this.r = r;
             this.c = c;
-            this.path = path;
         }
     }
 }

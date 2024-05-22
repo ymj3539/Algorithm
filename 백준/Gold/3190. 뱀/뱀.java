39a04a08c992ca99ac6a1044975f039cb02bad07
Minju Yoon <70592157+ymj3539@users.noreply.github.com>
2024-05-22 22:57:20 +0900

    [Gold IV] Title: 뱀, Time: 88 ms, Memory: 11752 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,127 +1,109 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-
+import java.io.*;
+import java.util.*;
 public class Main {
-	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer tokens;
-	
-	static int N, K, L;
-	static int[][] map;
-	static String[] dirInfo = new String[10001];
-	static int[] dx = {0, 1, 0, -1}; // 상(0) 우(1) 하(2) 좌(3)
-	static int[] dy = {-1, 0, 1, 0};
-	static int curDir;
-	static int answer;
-//	static Queue<Point> queue = new LinkedList<>();	// 뱀
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		N = Integer.parseInt(input.readLine());
-		K = Integer.parseInt(input.readLine());
-		
-		map = new int[N+1][N+1];
-		
-		for(int i=0; i<K; i++) {
-			tokens = new StringTokenizer(input.readLine());
-			int r = Integer.parseInt(tokens.nextToken());
-			int c = Integer.parseInt(tokens.nextToken());
-			
-			map[r][c] = 1;
-		}
-		
-		L = Integer.parseInt(input.readLine());
-		for(int i=0; i<L; i++) {
-			String[] str = input.readLine().split(" ");
-			dirInfo[Integer.parseInt(str[0])] = str[1];
-		}
-		
-//		System.out.println(Arrays.toString(dirInfo));
-		/////////////입력끝//////////////
-		
-		func();
-		System.out.println(answer);
-	}
-	
-	static void func() {
-		Queue<Point> queue = new LinkedList<>();
-		Point head = new Point(1,1);
-		queue.offer(new Point(1,1));
-		map[head.r][head.c] = 2;
-		curDir = 1;
-		
-		// 시간에 따른 처리
-		for(int i=1; i<dirInfo.length; i++) {
-			answer =i;
-//			for(int[] arr: map) {
-//				System.out.println(Arrays.toString(arr));
-//			}
-			
-			int nr = head.r + dy[curDir];
-			int nc = head.c + dx[curDir];
-			head.r = nr;
-			head.c = nc;
-//			System.out.println(i+"초(head):"+head.toString());
-			
-			
-			//// 진행하려는 곳에 사과의 유무에 따른 로직
-			if(!isIn(nr,nc)) {
-				return;
-				
-			}
-			
-			if(map[nr][nc] == 2) {
-				return;
-			}
-			
-			queue.offer(new Point(nr,nc));
-//			System.out.println(i+"초(peek):"+queue.peek());
-			
-			if(map[nr][nc] == 0) {
-				Point tail = queue.poll();
-				map[tail.r][tail.c] = 0;
-//				System.out.println(i+"초:"+tail.toString());
-			}
-			
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static int N, K, L; // N : 보드 크기, K : 사과 개수, L : 방향 전환 횟수
+    static int[][] map;
+    static Dir[] dir_arr;
+    static Deque<Point> Snake = new ArrayDeque<>();
+    static int[] S_dir = {0, 1};
+    public static void main(String[] args) throws Exception{
+        N = Integer.parseInt(input.readLine());
+        K = Integer.parseInt(input.readLine());
+        map = new int[N+1][N+1];
+        for(int i=0; i<K; i++){
+            tokens = new StringTokenizer(input.readLine());
+            int r = Integer.parseInt(tokens.nextToken());
+            int c = Integer.parseInt(tokens.nextToken());
 
-			map[nr][nc] = 2;
-			
-			
-			// 방향 전환
-			if(dirInfo[i] != null) {
-				if(dirInfo[i].equals("D")) {
-					curDir += 1;
-					
-				}else if(dirInfo[i].equals("L")) {
-					curDir += 3;
-				}
-				curDir %= 4;
-			}
-			
-		}
-	}
+            map[r][c] = 2; // 사과는 2로 표시
+        }
 
-	static class Point{
-		int r,c;
+        L = Integer.parseInt(input.readLine());
+        dir_arr = new Dir[L];
+        for(int i=0; i<L; i++){
+            tokens = new StringTokenizer(input.readLine());
+            int sec = Integer.parseInt(tokens.nextToken());
+            char dir = tokens.nextToken().charAt(0);
+            dir_arr[i] = new Dir(sec, dir);
+        }
 
-		public Point(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
+        Snake.offerFirst(new Point(1,1));
+        map[1][1] = 1;
 
-		@Override
-		public String toString() {
-			return "[(" + r + ", " + c + ")]";
-		}
-		
-		
-	}
-	
-	static boolean isIn(int r, int c) {
-		return (r>=1 && c>=1 && r<=N && c<=N);
-	}
+        int t = 1;
+        int dir_i = 0;
+        while(true){
+            // 머리 이동 가능한지 확인
+            Point Head = Snake.peekFirst();
+            int dr = Head.r + S_dir[0];
+            int dc = Head.c + S_dir[1];
+
+            // 벽이나 자기자신 몸과 부딪히면 끝
+            if(!isIn(dr, dc) || map[dr][dc] == 1){
+                break;
+            }
+
+            // 이동한 칸에 사과가 있다면
+            if(map[dr][dc] == 2){
+                map[dr][dc] = 1;
+            }
+
+            // 사과가 없다면
+            else {
+                // 꼬리칸 비우기
+                Point removed = Snake.removeLast();
+                map[removed.r][removed.c] = 0;
+            }
+
+            // 머리 진짜 이동
+            Snake.offerFirst(new Point(dr, dc));
+            map[dr][dc] = 1;
+
+
+            // 방향 전환
+            if(dir_i<L && t == dir_arr[dir_i].sec){
+                char dir = dir_arr[dir_i++].dir;
+                int y = S_dir[0];
+                int x = S_dir[1];
+                if(dir == 'L'){
+                    S_dir[0] = x*(-1);
+                    S_dir[1] = y;
+                }else{
+                    S_dir[0] = x;
+                    S_dir[1] = y*(-1);
+                }
+            }
+
+
+            t++;
+
+        }
+
+        System.out.println(t);
+    }
+
+    static boolean isIn(int r, int c){
+        return (r>=1 && r<=N && c>=1 && c<=N);
+    }
+
+    static class Point{
+        int r, c;
+        public Point(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+
+    static class Dir{
+        int sec;
+        char dir;
+        public Dir(int sec, char dir){
+            this.sec = sec;
+            this.dir = dir;
+        }
+
+    }
 }
